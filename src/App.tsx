db11cc87518d4f510648a14ac6ec48f80130229c
artyomromaniv <romaniv.artyom8@gmail.com>
2023-03-03 01:26:20 +0300

    localStorage with useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,43 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button} from './components/button/Button';
 import s from './App.module.css'
 
 
 export const App = () => {
 
-    const [count, setCount] = useState<number>(0)
+    const [value, setValue] = useState<number>(0)
+
+    useEffect(() => {
+        getCounterFromLocalStorage()
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('counterValue', JSON.stringify(value))
+    },[value])
 
     const increase = () => {
-        setCount(count + 1)
+        setValue(value + 1)
     }
     const reset = () => {
-        setCount(0)
+        setValue(0)
     }
-    const setCounterToLocalStorage = () => {
-        localStorage.setItem('counterValue', JSON.stringify(count))
-    }
+
     const getCounterFromLocalStorage = () => {
         let valueAsString = localStorage.getItem('counterValue')
         if (valueAsString) {
             let newValue = JSON.parse(valueAsString)
-            setCount(newValue)
+            setValue(newValue)
         }
     }
 
     return (
         <div className={s.App}>
             <div>
-                <h1 className={s.error ? 'error' : ''}>{count}</h1>
+                <h1 className={s.error}>{value}</h1>
             </div>
             <div className={s.buttonsBlock}>
-                <Button title={'Inc'} callback={increase} disabled={count === 5}/>
-                <Button title={'Reset'} callback={reset} disabled={count === 0}/>
-                <Button title={'Set'} callback={setCounterToLocalStorage} />
-                <Button title={'Get'} callback={getCounterFromLocalStorage} />
+                <Button title={'Inc'} callback={increase} disabled={value === 5}/>
+                <Button title={'Reset'} callback={reset} disabled={value === 0}/>
             </div>
         </div>
     )
