491516181a3bb8071db7ca5885185f79dbbc1ed8
artyomromaniv <romaniv.artyom8@gmail.com>
2023-03-03 19:17:34 +0300

    counter with set-menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React, {useEffect, useState} from 'react';
 import {Button} from './components/button/Button';
 import s from './App.module.css'
+import {Input} from "./components/input/Input";
 
 
 export const App = () => {
 
     const [value, setValue] = useState<number>(0)
+    const [initValue,setInitValue] = useState('')
+    const [maxValue,setMaxValue] = useState('')
 
     useEffect(() => {
         getCounterFromLocalStorage()
@@ -15,12 +18,6 @@ export const App = () => {
         localStorage.setItem('counterValue', JSON.stringify(value))
     },[value])
 
-    const increase = () => {
-        setValue(value + 1)
-    }
-    const reset = () => {
-        setValue(0)
-    }
 
     const getCounterFromLocalStorage = () => {
         let valueAsString = localStorage.getItem('counterValue')
@@ -30,14 +27,49 @@ export const App = () => {
         }
     }
 
+    const findInitValue = (newValue: string) => {
+        if (+newValue < 0) {
+            setInitValue('')
+        }
+        else setInitValue(newValue)
+    }
+
+    const findMaxValue = (newValue: string) => {
+        if (+newValue < 0) {
+            setMaxValue('')
+        }
+        else setMaxValue(newValue)
+    }
+
+    const increase = () => {
+        setValue(value + 1)
+    }
+    const reset = () => {
+        setValue(0)
+        setMaxValue('')
+        setInitValue('')
+    }
+
+    const setValueCount = () => {
+        setInitValue(initValue)
+        setMaxValue(maxValue)
+        setValue(+initValue)
+    }
+
+
     return (
         <div className={s.App}>
             <div>
-                <h1 className={s.error}>{value}</h1>
+                <h1 className={s.counter}>{value}</h1>
+            </div>
+            <div className={s.inputsBlock}>
+                <Input placeholder={'initial value'} callback={findInitValue} value={initValue}/>
+                <Input placeholder={'max value'} callback={findMaxValue} value={maxValue}/>
             </div>
             <div className={s.buttonsBlock}>
-                <Button title={'Inc'} callback={increase} disabled={value === 5}/>
+                <Button title={'Inc'} callback={increase} disabled={value === +maxValue}/>
                 <Button title={'Reset'} callback={reset} disabled={value === 0}/>
+                <Button title={'Set'} callback={setValueCount} />
             </div>
         </div>
     )
---
+++ src/components/input/Input.tsx
@@ -0,0 +1,26 @@
+import React, {ChangeEvent} from 'react';
+import s from './Input.module.css'
+
+
+type InputPropsType = {
+    placeholder: string
+    callback: (newValue: string) => void
+    value: string
+}
+
+export const Input = (props: InputPropsType) => {
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        props.callback(e.currentTarget.value)
+    }
+    return (
+        <div className={s.inputs}>
+            <input
+                className={s.input}
+                placeholder={props.placeholder}
+                onChange={onChangeHandler}
+                value={props.value}
+            />
+        </div>
+    )
+}
